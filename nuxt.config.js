3a5d428091abaa99b9db4bf39ce0c6eff33e0fdd
David van den Pol <david@mooore.nl>
2022-10-01 14:33:25 +0200

    IMP: Nuxt version & index

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,88 +1,89 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
+// Global page headers: https://go.nuxtjs.dev/config-head
+head: {
     title: 'David van den Pol - Frontend Web Developer - Portfolio',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'robots', content: 'index, follow' },
-      { hid: 'description', name: 'description', content: "I'm David van den Pol a Frontend Web Developer. I'm a Frontend Web Developer for a couple years. David van den Pol - Frontend Web Developer - Portfolio" },
-      { name: 'format-detection', content: 'telephone=no' },
-      { name: "viewport", content: "width=device-width, initial-scale=1.0" },
-      { property: "og:locale", content: "en_US" },
-      { property: "og:type", content: "website" },
-      { property: "og:title", content: "David van den Pol - Frontend Web Developer - Portfolio" },
-      { property: "og:description", content: "I'm David van den Pol a Frontend Web Developer. I'm a Frontend Web Developer for a couple years. David van den Pol - Frontend Web Developer - Portfolio" },
-      { property: "og:url", content: "https://davidvandenpol.nl/" },
-      { property: "og:site_name", content: "David van den Pol: Frontend Web Developer Portfolio" },
-      { name: "twitter:card", content: "summary_large_image" },
-      { name: "twitter:title", content: "David van den Pol - Frontend Web Developer - Portfolio" },
-      { name: "twitter:description", content: "Neoistone is a reliable, fast and robust platform for all the needs of Hosting and Domain name development tools is so easy to use that litespeed it makes fast site" },
-      { name: "twitter:image", content: "https://davidvandenpol.nl/_nuxt/img/davidvandenpol-face.6d58ddb.svg" },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'robots', content: 'index, follow' },
+        { hid: 'description', name: 'description', content: "I'm a Front-end developer specialized in building exceptional digital websites. Currently, I'm concentrating on my carrier at Mooore Digital." },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        { property: "og:locale", content: "en_US" },
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: "David van den Pol - Frontend Web Developer - Portfolio" },
+        { property: "og:description", content: "I'm a Front-end developer specialized in building exceptional digital websites. Currently, I'm concentrating on my carrier at Mooore Digital." },
+        { property: "og:url", content: "https://davidvandenpol.nl/" },
+        { property: "og:site_name", content: "David van den Pol: Frontend Web Developer Portfolio" },
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "David van den Pol - Frontend Web Developer - Portfolio" },
+        { name: "twitter:description", content: "I'm a Front-end developer specialized in building exceptional digital websites. Currently, I'm concentrating on my carrier at Mooore Digital." },
+        { name: "twitter:image", content: "https://davidvandenpol.nl/_nuxt/img/davidvandenpol-face.6d58ddb.svg" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'canonical', href: 'https://www.davidvandenpol.nl/' }
     ]
-  },
+},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+// Global CSS: https://go.nuxtjs.dev/config-css
+css: [
+],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+plugins: [
+],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+// Auto import components: https://go.nuxtjs.dev/config-components
+components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
+// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     'nuxt-compress'
-  ],
+],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
+// Modules: https://go.nuxtjs.dev/config-modules
+modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     'nuxt-compress',
-  ],
+],
 
-    'nuxt-compress': {
-        gzip: {
-            threshold: 8192,
+'nuxt-compress': {
+    gzip: {
+        threshold: 8192,
+    },
+    brotli: {
+        threshold: 8192,
+    },
+},
+
+// Axios module configuration: https://go.nuxtjs.dev/config-axios
+axios: {
+// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+baseURL: '/',
+},
+
+// PWA module configuration: https://go.nuxtjs.dev/pwa
+pwa: {
+    manifest: {
+        lang: 'en'
+    }
+},
+
+// Build Configuration: https://go.nuxtjs.dev/config-build
+build: {
+    loaders: {
+        sass: {
+            implementation: require('sass'),
         },
-        brotli: {
-            threshold: 8192,
+        scss: {
+            implementation: require('sass'),
         },
     },
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
-
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
-    }
-  },
-
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {
-        loaders: {
-            sass: {
-                implementation: require('sass'),
-            },
-            scss: {
-                implementation: require('sass'),
-            },
-        },
-    }
+}
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,30 @@
 <template>
-    <div>
-        <Navbar/>
-        <Wordpress :data="pages"/>
-        <Footer/>
-    </div>
+	<div>
+		<Navbar />
+		<Wordpress :data="pages" />
+		<Footer />
+	</div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    async mounted() {
-        await axios.get('https://davidvandenpol.nl/wordpress/wp-json/wp/v2/pages/2')
-            .then(res => {
-                this.$store.commit('setPages', res.data.content.rendered);
-            });
-    },
-    async fetch({ store }) {
-        return await axios.get('https://davidvandenpol.nl/wordpress/wp-json/wp/v2/pages/2')
-            .then(res => {
-                store.commit('setPages', res.data.content.rendered);
-            });
-    },
-    computed: {
-        pages() {
-            return this.$store.state.pages;
-        }
-    },
-}
+	async fetch({ store }) {
+		return await axios
+			.get("https://davidvandenpol.nl/wordpress/wp-json/wp/v2/pages/2")
+			.then((res) => {
+				store.commit("setPages", res.data.content.rendered);
+			});
+	},
+	computed: {
+		pages() {
+			return this.$store.state.pages;
+		},
+	},
+};
 </script>
 
 <style lang="scss">
-    @import '../styles/main.scss';
+@import "../styles/main.scss";
 </style>
