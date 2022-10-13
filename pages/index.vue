d68f3bae107168b226a6834bd5dadcd633dd1b38
David van den Pol <david.vanden.pol@gmail.com>
2022-10-13 22:24:59 +0200

    IMP: data fetch

--- pages/index.vue
+++ pages/index.vue
@@ -10,18 +10,23 @@
 import axios from "axios";
 
 export default {
-	async fetch({ store }) {
-		return await axios
-			.get("https://davidvandenpol.nl/wordpress/wp-json/wp/v2/pages/2")
-			.then((res) => {
-				store.commit("setPages", res.data.content.rendered);
-			});
-	},
-	computed: {
-		pages() {
-			return this.$store.state.pages;
-		},
-	},
+	async mounted() {
+        await axios.get('https://davidvandenpol.nl/wordpress/wp-json/wp/v2/pages/2')
+            .then(res => {
+                this.$store.commit('setPages', res.data.content.rendered);
+            });
+    },
+    async fetch({ store }) {
+        return await axios.get('https://davidvandenpol.nl/wordpress/wp-json/wp/v2/pages/2')
+            .then(res => {
+                store.commit('setPages', res.data.content.rendered);
+            });
+    },
+    computed: {
+        pages() {
+            return this.$store.state.pages;
+        }
+    },
 };
 </script>
 
