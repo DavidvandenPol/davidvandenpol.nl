fa2299f48c3741a56bc2dda6ce7a9256a6a0ce31
David van den Pol <david.vanden.pol@gmail.com>
2022-10-20 21:13:48 +0200

    IMP: Navbar

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,48 +1,48 @@
 <template>
 	<nav aria-label="quick menu" class="py-3.5 z-10 bg-opacity-30">
 		<div class="px-2 mx-auto flex items-center container">
-			<nuxt-link to="/">
+			<NuxtLink to="/">
 				<div class="flex flex-col justify-end sm:justify-center">
 					<span class="leading-5 text-xl font-bold">David</span>
 				</div>
-			</nuxt-link>
+			</NuxtLink>
 			<div class="m-auto z-10">
-				<nuxt-link
+				<NuxtLink
                     to="/works"
 					class="inline-flex mx-2 md:mx-6 text-sm"
 					aria-label="View all of David van den Pol's Works"
 				>
 					<span bis_skin_checked="1">Works</span>
-				</nuxt-link>
-				<nuxt-link
+				</NuxtLink>
+				<NuxtLink
                     to="/blog"
 					class="inline-flex mx-2 md:mx-6 text-sm"
 					aria-label="Read David van den Pol's Blog"
 				>
 					<span bis_skin_checked="2">Blog</span>
-				</nuxt-link>
-				<nuxt-link
+				</NuxtLink>
+				<NuxtLink
                     to="/about"
 					class="inline-flex mx-2 md:mx-6 text-sm"
 					aria-label="About David van den Pol"
 				>
 					<span bis_skin_checked="3">About me</span>
-				</nuxt-link>
-				<nuxt-link
+				</NuxtLink>
+				<NuxtLink
                     to="/socialmedia"
 					class="hidden sm:inline-flex mx-2 md:mx-6 text-sm"
 					aria-label="Social Media of David van den Pol"
 				>
 					<span bis_skin_checked="4">Social Media</span>
-				</nuxt-link>
+				</NuxtLink>
 			</div>
-			<nuxt-link
+			<NuxtLink
                 to="/letstalk"
 				class="inline-flex justify-center mx-2 md:ml-6 text-textColor font-bold text-sm"
 				aria-label="Contact David van den Pol"
 			>
 				<span bis_skin_checked="4" class="mr-2">Let's talk</span>
-			</nuxt-link>
+			</NuxtLink>
 		</div>
 	</nav>
 </template>
